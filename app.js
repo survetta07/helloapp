f65b6f2e43d1a42be28603ebed9dbf64e2306d4d
black <black@example.com>
2026-07-02 00:39:06 +0500

    Add greetBlack function

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-console.log("hello"); 
-
+// Весь ваш старый код (функция greetWhite и т.д.) остается на месте
 function greetWhite() {
-    console.log("hello white");
+    console.log("hello white"); // или то, что там было написано у White
 }
+
 console.log("hello everyone");
+
+// Вы просто дописываете в самый конец новую функцию:
+function greetBlack() {
+    console.log("hello black");
+}
