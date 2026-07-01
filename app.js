1c78ab7686326197d338d7f86fa04fa8209369e2
black <black@example.com>
2026-07-02 01:11:43 +0500

    Fix code: call functions

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-// Весь ваш старый код (функция greetWhite и т.д.) остается на месте
-function greetWhite() {
-    console.log("hello white"); // или то, что там было написано у White
-}
-
 console.log("hello everyone");
 
-// Вы просто дописываете в самый конец новую функцию:
+greetWhite();
+greetBlack();
+
+function greetWhite() {
+    console.log("hello white");
+}
+
 function greetBlack() {
     console.log("hello black");
 }
